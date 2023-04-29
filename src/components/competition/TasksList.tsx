577eb8eb4e6eebe4dda2149cdb7298f2aa8c09b4
Pashs-ba <pashs.ba@gmail.com>
2023-04-29 13:38:03 +0300

    make normal links

--- src/components/competition/TasksList.tsx
+++ src/components/competition/TasksList.tsx
@@ -1,16 +1,27 @@
 import {Problem} from "../../types/schema";
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import {RouterName} from "../../router";
+import "../../style/base.sass"
 
 interface TasksListInterface {
     problems: Problem[]
 }
 
 export function TasksList({problems}: TasksListInterface) {
-    const RenderProblems = (problems: Problem[])=>{
+    const RenderProblems = (problems: Problem[]) => {
         let elements = []
-        for(let i = 0; i<problems.length; i++){
-            elements.push((<li className="list-group-item" key={i}><Link to={RouterName.ProblemPage(problems[i].id)}>{problems[i].name}</Link></li>))
+        for (let i = 0; i < problems.length; i++) {
+            elements.push((
+                <li className="list-group-item" key={i}>
+                    <NavLink
+                        className={({isActive}) =>
+                            isActive ? "normal_link" : "text-secondary normal_link"
+                        }
+                        to={RouterName.ProblemPage(problems[i].id)}>
+                        {problems[i].name}
+                    </NavLink>
+                </li>
+            ))
         }
         return elements
     }
